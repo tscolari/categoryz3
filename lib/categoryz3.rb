bd7f1916dcaf285d29112aed9038ff8ce4efa246
Tiago Scolari <tscolari@gmail.com>
2012-10-06 01:11:05 -0300

    Fixing the require path for the child_item_category_validator

--- lib/categoryz3.rb
+++ lib/categoryz3.rb
@@ -1,6 +1,6 @@
 require "categoryz3/engine"
 require "categoryz3/categorizable"
-require "Categoryz3/validators/child_item_category_validator"
+require "categoryz3/validators/child_item_category_validator"
 
 module Categoryz3
 end
