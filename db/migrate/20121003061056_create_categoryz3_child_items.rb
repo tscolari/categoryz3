b78138bdce5591107ce942ddac39e6032b038658
Tiago Scolari <tscolari@gmail.com>
2012-10-07 16:19:09 -0300

    Unique index in child_item must include master_item_id.

--- db/migrate/20121003061056_create_categoryz3_child_items.rb
+++ db/migrate/20121003061056_create_categoryz3_child_items.rb
@@ -7,7 +7,7 @@ class CreateCategoryz3ChildItems < ActiveRecord::Migration
       t.timestamps
     end
     add_index :categoryz3_child_items, [:category_id, :created_at]
-    add_index :categoryz3_child_items, [:category_id, :categorizable_type, :categorizable_id], unique: true, name: 'child_items_unq_idx'
+    add_index :categoryz3_child_items, [:category_id, :categorizable_type, :categorizable_id, :master_item_id], unique: true, name: 'child_items_unq_idx'
     add_index :categoryz3_child_items, [:categorizable_type, :categorizable_id], name: 'child_items_categorizable_idx'
     add_index :categoryz3_child_items, [:master_item_id]
   end
